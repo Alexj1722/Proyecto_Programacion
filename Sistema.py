9f3ef22633b7895bfe90ed7debb521a2581a612c
Alexj1722 <Alexlemus1722@gmail.com>
2020-03-28 09:53:03 -0600

    Avance del diseño

--- Sistema.py
+++ Sistema.py
@@ -8,9 +8,12 @@
 #Fecha: 27/03/2020
 
 import sqlite3
+from sqlite3 import Error
+import os
+import sys
 from PyQt5.QtGui import QFont, QIcon, QPixmap
-from PyQt5.QtCore import Qt, QTime, QTimer
 from PyQt5.QtWidgets import *
+import qdarkstyle
 
 cliente_id = None
 
@@ -18,19 +21,48 @@ class Main(QWidget):
     """ Ventana principal del programa """
     def __init__(self):
         super().__init__()
+        #empezamos creando o abriendo la conexion a la base de datos
+        self.cliente_db = Cliente_DB("Clientes.db")
         self.windowTitle("Gestion de clientes")
-        self.setGeometry(550,250,850,700)
         self.UI()
         self.show()
-        # Crear conecion a la base de datos"
         
+    def UI(self):
+        """Creacion de los objetos que componen la interfaz del programa"""
+        self.main_design()
+        self.layouts()
+    
+    def main_design(self):
+        """Diseño del formulario principal"""
+        self.lista_Clientes = QListWidget()
+        self.btn_nuevo = QPushButton("Nuevo")
+        self.btn_Actualizar = QPushButton("Actualizar")
+        self.btn_Eliminar = QPushButton("Eliminar")
         
-
+    def layouts(self):
+        """Layouts que componen el programa"""
+        self.main_layout = QHBoxLayout()
+        self.left_layout = QFormLayout()
+        self.right_main_layout = QVBoxLayout()
+        self.right_top_layout = QHBoxLayout()
+        self.right_bottom_layout = QHBoxLayout()
+        
+        #Agregar los layout hijos al padre
+        self.right_main_layout.addLayout(self.right_top_layout)
+        self.right_main_layout.addLayout(self.right_bottom_layout)
+        self.main_layout.addLayout(self.left_layout,50)
+        self.main_layout.addLayout(self.right_main_layout,70)
+        
+        #Colocar el layout principal en la ventana principal
+        self.setLayout(self.main_layout)
 
 def main():
     app = QApplication(sys.argv)
     window = Main()
+    window.showMaximized()
     sys.exit(app.exec_())
     
-if --name-- == "__main__":
+if __name__ == "__main__":
+    
     main()
+   
